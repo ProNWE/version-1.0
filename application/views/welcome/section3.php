8a64ecde6370ecea8b65f526e334b5356906cc0d
unvir <unvir@yandex.ru>
2015-09-16 23:14:00 +0300

    Update section3.php

--- application/views/welcome/section3.php
+++ application/views/welcome/section3.php
@@ -1,8 +1,8 @@
 <video autoplay loop muted id="myVideo">
-			<source src="<?=$assets; ?>imgs/flowers.mp4" type="video/mp4">
-			<source src="<?=$assets; ?>imgs/flowers.webm" type="video/webm">
-		</video>
-		<div class="layer">
-			<h2 style="color: #fff">Здесь будет представлены основные функции, поддерживаемые сайтом для разных ролей (организатор,жюри,гость).<br>
-			Будет нарисовано после создания соответствующей страницы (Коля)</h2>
-		</div>
+	<source src="<?=$assets; ?>imgs/flowers.mp4" type="video/mp4">
+	<source src="<?=$assets; ?>imgs/flowers.webm" type="video/webm">
+</video>
+<div class="layer">
+	<h2 style="color: #fff">Здесь будет представлены основные функции, поддерживаемые сайтом для разных ролей (организатор,жюри,гость).<br>
+	Будет нарисовано после создания соответствующей страницы (Коля)</h2>
+</div>
